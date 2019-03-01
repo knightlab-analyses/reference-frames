fc841901104ca4271975d8c702dc28eddf5d9331
Jamie Morton <jamietmorton@gmail.com>
2019-03-01 14:05:54 -0500

    Update soils-multinomial.sh

--- scripts/soils-multinomial.sh
+++ scripts/soils-multinomial.sh
@@ -5,7 +5,7 @@ songbird multinomial \
 	--input-biom ../data/soils/ref_table.biom \
 	--metadata-file ../data/soils/metadata.txt \
 	--batch-size 20 \
-	--epoch 10000 \
+	--epochs 10000 \
     	--learning-rate 1e-3 \
     	--min-feature-count 24 \
 	--summary-dir $results_dif
